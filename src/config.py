a87f2a6f8a27ff645e5b1558c5cc77c6914ce4b1
Amogh J <amoghjahagirdargsf@gmail.com>
2020-04-17 19:31:17 +0530

    Initial Commit

--- src/config.py
+++ src/config.py
@@ -5,8 +5,8 @@ MAX_LEN = 512
 TRAIN_BATCH_SIZE = 8
 VALID_BATCH_SIZE = 4
 EPOCHS = 10
-BERT_PATH = "../input/bert_base_uncased/"
-MODEL_PATH = "model.bin"
+BERT_PATH = "../input/bert-based-uncased/"
+MODEL_PATH = "../input/pytorch_model.bin"
 TRAINING_FILE = "../input/imdb.csv"
 TOKENIZER = transformers.BertTokenizer.from_pretrained(
     BERT_PATH,
